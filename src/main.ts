276655875cc3f72140afb721d6deb7e5149f7dd3
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2020-03-29 16:24:17 +0900

    add main bootstrap script

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import crawl from '#/Bootstrap/Crawl';
+import report from '#/Bootstrap/Report';
+import provideOptions from './Provider/MainOptionsProvider';
+
+(async () => {
+    const mode = provideOptions();
+    switch (mode) {
+        case 'crawl':
+            await crawl();
+            break;
+        case 'report':
+            await report();
+            break;
+    }
+})();
