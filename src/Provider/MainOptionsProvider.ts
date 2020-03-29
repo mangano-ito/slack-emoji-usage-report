401aae958a6094605bbfff65ceea95a2810e89fe
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2020-03-29 15:54:10 +0900

    add main option provider

---
+++ src/Provider/MainOptionsProvider.ts
@@ -0,0 +1,19 @@
+import commandLineArgs, { OptionDefinition } from 'command-line-args';
+
+const optionDefinitions: OptionDefinition[] = [
+    {
+        name: 'mode',
+        type: String,
+        defaultOption: true,
+    },
+];
+
+export default (): ('crawl' | 'report') => {
+    const options = commandLineArgs(optionDefinitions, { partial: true });
+    const mode = options.mode;
+    if (!mode) {
+        throw new Error(`mode is required and must be "crawl" or "report".`);
+    }
+
+    return mode;
+};
