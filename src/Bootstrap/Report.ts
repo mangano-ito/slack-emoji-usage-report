2d8c7c93ac987422acca5df65f91408cc4ddbeab
Asato Nago <nago@tam-bourine.co.jp>
2021-01-17 00:40:16 +0900

    refactor: 💡 make it wokrks w/o CLI options

--- src/Bootstrap/Report.ts
+++ src/Bootstrap/Report.ts
@@ -1,7 +1,9 @@
+import * as config from "../config";
+
 import Reporter from "#/Controller/Reporter";
 import provideOptions from "#/Provider/ReportOptionsProvider";
 
 export default async() => {
     const options = provideOptions();
-    await new Reporter(options.database_name).run();
+    await new Reporter(options.database_name || config.SQLite.DB_NAME).run();
 };
--- src/Provider/ReportOptionsProvider.ts
+++ src/Provider/ReportOptionsProvider.ts
@@ -1,4 +1,5 @@
 import commandLineArgs, { OptionDefinition } from 'command-line-args';
+import * as config from "../config";
 
 import ReportOptions from "#/Model/ReportOptions";
 
@@ -13,5 +14,5 @@ const optionDefinitions: OptionDefinition[] = [
 export default (): ReportOptions => {
     const options = commandLineArgs(optionDefinitions, { partial: true });
 
-    return {database_name: options.db};
+    return {database_name: options.db || config.SQLite.DB_NAME};
 };
